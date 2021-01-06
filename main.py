e8c9cf54410caf60106227331ef7ef44b5869218
Holiday Man <lesha.pitenko791@gmail.com>
2021-01-06 21:26:57 +0200

    added config

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import os
+
 import telethon
 import configparser
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+
+if os.path.exists('config_prod.ini'):
+    config.read('config_prod.ini')
+else:
+    config.read('config.ini')
 
 api_id = config['user_bot']['api_id']
 api_hash = config['user_bot']['api_hash']
 
+client = TelegramClient('anon', api_id, api_hash)
 
